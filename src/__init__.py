1a5c0ca5cdeae2bf22283271c688165d8179a648
emilio <emivaldigle@gmail.com>
2025-03-19 01:17:51 -0300

    fix logging

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,16 @@
 import logging
 import logging.handlers
+import queue  # Para usar QueueHandler
 
+# Crear una cola para los logs
+log_queue = queue.Queue()
+
+# Configuración de logging
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.handlers.QueueHandler(logging.handlers.QueueHandler()),
+        logging.handlers.QueueHandler(log_queue),  # Usando la cola para el QueueHandler
         logging.FileHandler("/plate_recognition.log", mode='a'),
     ]
 )
